3812cabe0a5f510ce8fbbb2f6683b07b3c150bea
Ashish Poudel <ashishpoudel523@gmail.com>
2022-12-08 16:40:25 +0545

    HB CRUD with Annotations

---
+++ src/main/java/com/ashish/deom/StudentDao.java
@@ -0,0 +1,134 @@
+package com.ashish.deom;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class StudentDao implements IStudentDao {
+
+    // save Student
+    // get All Students
+    // get Student By Id
+    // Update Student
+    // Delete Student
+
+    /* (non-Javadoc)
+     * @see net.javaguides.hibernate.dao.IStudentDao#saveStudent(net.javaguides.hibernate.model.Student)
+     */
+    @Override
+    public void saveStudent(Student student) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start the transaction
+            transaction = session.beginTransaction();
+
+            // save student object
+            session.save(student);
+
+            // commit the transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see net.javaguides.hibernate.dao.IStudentDao#updateStudent(net.javaguides.hibernate.model.Student)
+     */
+    @Override
+    public void updateStudent(Student student) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start the transaction
+            transaction = session.beginTransaction();
+
+            // save student object
+            session.saveOrUpdate(student);
+
+            // commit the transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see net.javaguides.hibernate.dao.IStudentDao#getStudentById(long)
+     */
+    @Override
+    public Student getStudentById(long id) {
+        Transaction transaction = null;
+        Student student = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start the transaction
+            transaction = session.beginTransaction();
+
+            // get student object
+            student = session.get(Student.class, id);
+            //student = session.load(Student.class, id);
+            // commit the transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        return student;
+    }
+
+    /* (non-Javadoc)
+     * @see net.javaguides.hibernate.dao.IStudentDao#getAllStudents()
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public List < Student > getAllStudents() {
+        Transaction transaction = null;
+        List < Student > students = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start the transaction
+            transaction = session.beginTransaction();
+
+            // get students
+            students = session.createQuery("from Student").list();
+            //student = session.load(Student.class, id);
+            // commit the transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        return students;
+    }
+
+    /* (non-Javadoc)
+     * @see net.javaguides.hibernate.dao.IStudentDao#deleteStudent(long)
+     */
+    @Override
+    public void deleteStudent(long id) {
+        Transaction transaction = null;
+        Student student = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start the transaction
+            transaction = session.beginTransaction();
+
+            student = session.get(Student.class, id);
+            // get student object
+            session.delete(student);
+            //student = session.load(Student.class, id);
+            // commit the transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+}
